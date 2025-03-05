1b5ce026bebe66210463da87abe233c0eb45a2ce
Violeta Velandria <velandriaa9@gmail.com>
2025-03-05 13:07:32 +0000

    miauuu

--- get_next_line.c
+++ get_next_line.c
@@ -26,47 +26,32 @@ char	*get_next_line(int fd)
 	static char	*cache;
 	int			leido;
 	char		texto[BUFF_SIZE + 1];
+	char		*new_line;
 	char		*linea;
 
-	if (fd < 0 || BUFF_SIZE <= 0)
+	if (fd < 0 || BUFF_SIZE <= 0)				// si el error con fd o buffer negativo
 		return (NULL);
-	new_line = ft_strchr(cache, '\n');
-	while (new_line = NULL)
+	new_line = ft_strchr(cache, '\n');			// buscar si la línea tiene un salto de línea, retorna nulo si no
+	while (new_line == NULL)					// mientras que no se encuentre el salto de línea
 	{
-        leido = read(fd, texto, BUFF_SIZE);
-		if (leido <= 0)
+        leido = read(fd, texto, BUFF_SIZE);		// leer conforme el buffer lo permita
+		if (leido <= 0)							// si EOF (0) o error (-1)
 		{
-			if (leido == 0 && cache != NULL)
+			if (leido == 0 && cache != NULL)	// si es EOF y la caché N0 está vacía
 			{
-				linea = ft_strdup(cache);
-				free(cache);
-				cache = NULL;
-				return (linea)
+				linea = ft_strdup(cache);		// copiar la caché a la línea
+				free(cache);					// liberar caché
+				cache = NULL;	// por qué ??
+				return (linea)					// retornar la línea
 			}
-			return (NULL);
+			return (NULL);						// caso contrario, la caché sí estaba vacía y EOF
 		}
-		texto[leido] = '\0';
-		cache = ft_strjoin(cache, texto);
+		texto[leido] = '\0';					// añadir carácter nulo al final del texto leido
+		cache = ft_strjoin(cache, texto);		// concatenar caché y texto
 		if (!cache)
-		{
 			return (NULL);
-			free(cache);
-		}
-		if (is_new_line(&cache))
-			linea = ft_strchr(cache, '\n');
-	}
-	if (!cache)
-		return (NULL);
-	else
-		return (cache);
+		new_line = ft_strchr(cache, '\n');	// Buscar salto de línea nuevamente
+		
 	return (linea);
 	free(cache);
-}
-
-int	is_new_line(char str)
-{
-	if (str == '\n')
-		return (0);
-	else
-		return (1);
 }
